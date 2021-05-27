270bb9fab933c56e7a5968ae08cbe47e33ec5cab
oyinnijesu <oyinnijesudeborah22@gmail.com>
2021-05-27 11:42:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+let menuIcon = document.querySelector(".menu_icon")
+let floatingMenu = document. querySelector(".floating_menu")
+
+
+menuIcon.onclick = ()=>{
+    
+    floatingMenu.style.visibility = 'visible'
+    
+
+    floatingMenu.classList.toggle('bevisible');
+}
+
